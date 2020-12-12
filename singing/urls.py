fc36c4c9af53bd2e05567b4e580752cdc0aaa66e
Leno <akrachunov@gmail.com>
2020-12-12 15:36:11 +0000

    Recover settings setup.

--- singing/urls.py
+++ singing/urls.py
@@ -22,4 +22,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', include('songs.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
